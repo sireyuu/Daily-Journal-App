12380ef3ce382a537b1df1a7e8b57a7344eedb0e
izskat <73699431+izskat@users.noreply.github.com>
2022-12-12 13:48:07 +0800

    pushh

---
+++ MC3-20221212T051532Z-001/MC3/Initial Files/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+app.set("view engine", "ejs");
+
+const homeStartingContent = "Good luck on your Final Exam. Merry Christmas and Happy New Year to Everyone!";
+const aboutContent = "Good luck on your Final Exam. Merry Christmas and Happy New Year to Everyone!";
+const contactContent = "Good luck on your Final Exam.Merry Christmas and Happy New Year to Everyone!";
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = new mongoose.Schema({ title: String, content: String }, { timestamps: true });
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.listen(3000, function () {
+    console.log("server started on port 3000");
+  });
+
